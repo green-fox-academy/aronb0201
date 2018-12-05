1763d4e27382b61e382f8c34d7d83d8595e70355
AronBaracs <aron.baracs@gmail.com>
2018-12-05 12:59:29 +0100

    exercise 04

---
+++ week_06/two_numbers/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+
+    int a = 13;
+    int b = 22;
+
+    int add = a + b;
+    int subs = b - a;
+    int mult = b * a;
+    float div = b / a;
+    int rem = b % a;
+
+
+    printf("%d\n", add);
+    printf("%d\n", subs);
+    printf("%d\n", mult);
+    printf("%f.2\n", div);
+    printf("%d\n", rem);
+            printf("Hello, World!\n");
+    return 0;
+}
