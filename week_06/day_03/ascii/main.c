dc8c2484c234dd558e48d4f46cf458630c24aa89
AronBaracs <aron.baracs@gmail.com>
2018-12-07 16:44:50 +0100

    ex 07

--- week_06/day_03/ascii/main.c
+++ week_06/day_03/ascii/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int array[] = { 103, 114, 101, 101, 110, 32, 102, 111, 120};
+
+
+    for (int i = 0; i <9 ; ++i) {
+        printf("%c", array[i]);
+    }
+    
+
+
     return 0;
 }
