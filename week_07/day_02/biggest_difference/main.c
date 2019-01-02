d7720d3c32e5a86c518b8da1c934dbc652c59ac3
aronb0201 <aron.baracs@gmail.com>
2019-01-02 15:31:01 +0100

    upgraded average question of exercise biggest_difference

--- week_07/day_02/biggest_difference/main.c
+++ week_07/day_02/biggest_difference/main.c
@@ -2,48 +2,47 @@
 #include <stdlib.h>
 
 #include <string.h>
-typedef struct resoults{
 
-}resoult_t;
+typedef struct resoults {
+
+} resoult_t;
 
 
 typedef struct student {
     float exams;
 
-}student_t;
+} student_t;
 
-typedef struct class{
+typedef struct class {
     int student_number;
     char class_name[3];
-    struct student* students;
+    struct student *students;
 
-}class_t;
-struct class* clases;
-struct resoult * resoults_array;
+} class_t;
+struct class *clases;
+struct resoult *resoults_array;
 
 
-float average_of_all_exam(struct class* clases);
+float average_of_all_exam(struct class *clasesn, int number_of_classes);
+
 int main() {
     int class_number;
-    int counter = 0;
-    float average;
-
 
 
     printf("how many classes took the exam?\n");
-    scanf("%d",&class_number );
-    clases= malloc(class_number* sizeof(struct class));
+    scanf("%d", &class_number);
+    clases = malloc(class_number * sizeof(struct class));
 
-    for (int i = 0; i <class_number ; ++i) {
+    for (int i = 0; i < class_number; ++i) {
         printf("name of the class %d ? \n", i);
         scanf("%s", clases[i].class_name);
 
         printf("how many students took the exam of class %s ?\n", clases[i].class_name);
         scanf("%d", &clases[i].student_number);
-        clases[i].students = malloc(clases[i].student_number* sizeof(struct student));
+        clases[i].students = malloc(clases[i].student_number * sizeof(struct student));
 
-        for (int j = 0; j <clases[i].student_number ; ++j) {
-            resoults_array = malloc(clases[i].student_number*sizeof(struct resoults));
+        for (int j = 0; j < clases[i].student_number; ++j) {
+            resoults_array = malloc(clases[i].student_number * sizeof(struct resoults));
             printf("give me the percentage of the exams in class %s os student %d \n", clases[i].class_name, j);
             scanf("%f", &clases[i].students[j].exams);
 
@@ -51,31 +50,36 @@ int main() {
 
 
     }
-    for (int k = 0; k <class_number ; ++k) {
+    for (int k = 0; k < class_number; ++k) {
         printf("name of class %s students are in it %d\n", clases[k].class_name, clases[k].student_number);
 
-        for (int j = 0; j <clases[k].student_number ; ++j) {
-            printf("exam resoults are %f \n",clases[k].students[j].exams);
+        for (int j = 0; j < clases[k].student_number; ++j) {
+            printf("exam resoults are %f \n", clases[k].students[j].exams);
 
         }
 
 
     }
 
-    printf("average iz %f", average_of_all_exam(clases));
+    printf("average iz %f", average_of_all_exam(clases, class_number));
 
     return 0;
 }
 
-float average_of_all_exam(struct class* clases){
-    float average;
-    for (int i = 0; i < sizeof(clases)/ sizeof(*clases); ++i) {
-        for (int j = 0; j <clases[i].student_number ; ++j) {
-            average =+ clases[i].students[i].exams;
+float average_of_all_exam(struct class *clases, int number_of_classes) {
+    float sum = 0;
+    int number_of_pepole = 0;
 
+    for (int i = 0; i < number_of_classes; ++i) {
+        number_of_pepole += clases[i].student_number;
+    }
+    for (int j = 0; j < number_of_classes; ++j) {
+        for (int i = 0; i < clases[j].student_number; ++i) {
+            sum += clases[j].students[i].exams;
         }
-        return (average/clases[i].student_number);
-
 
     }
+    return sum / (float) number_of_pepole;
 }
+
+
