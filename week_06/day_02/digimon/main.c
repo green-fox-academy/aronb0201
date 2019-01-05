f36b09c31d14bc71eb6f70904942334b36c88093
aronb0201 <aron.baracs@gmail.com>
2019-01-05 15:20:52 +0100

    digimon ex upgrade

--- week_06/day_02/digimon/main.c
+++ week_06/day_02/digimon/main.c
@@ -21,16 +21,21 @@ typedef struct digimon {
 
 } digimon_t;
 
-struct digimon digimon_array[50];
+//struct digimon digimon_array[50];
 int array_length = 0;
 
-int get_minimum_hp(struct digimon digimon_array[array_length], int array_size);
 
-int same_digivolution_level(struct digimon *digimon_array, int array_size, enum digivolution_level digivolutionLevel);
+int get_minimum_hp(digimon_t *digimon_array, int array_size);
 
-int same_trainer(struct digimon *digimon_array, int array_size, char tamer_name[256]);
+int same_digivolution_level(digimon_t *digimon_array, int array_size, enum digivolution_level digivolutionLevel);
+
+int same_trainer(digimon_t *digimon_array, int array_size, char tamer_name[256]);
+
+int average_hp_of_same_trainer(digimon_t *digimon_array, int array_size, char tamer_name[256]);
 
 int main() {
+
+    digimon_t digimons[4];
 
     struct digimon digimon1;
     strcpy(digimon1.name, "digigeci");
@@ -38,7 +43,7 @@ int main() {
     digimon1.health = 100;
     strcpy(digimon1.tamer_name, "senkihazi");
     digimon1.digivolutionLevel = ROOKIE;
-
+    digimons[0] = digimon1;
     struct digimon digimon2;
 
     strcpy(digimon1.name, "digikocsog");
@@ -47,7 +52,7 @@ int main() {
     strcpy(digimon2.tamer_name, "valamigyoker");
 
     digimon2.digivolutionLevel = MEGA;
-
+    digimons[1] = digimon2;
 
     struct digimon digimon3;
 
@@ -56,55 +61,93 @@ int main() {
     digimon3.health = 47;
     strcpy(digimon3.tamer_name, "mittudomen");
     digimon3.digivolutionLevel = CHAMPION;
+    digimons[2] = digimon3;
 
 
-    printf("%d\n", get_minimum_hp(&digimon_array[array_length], 3));
+    struct digimon digimon4;
 
-    printf("%d\n", same_digivolution_level(&digimon_array[array_length], 3, ROOKIE));
+    strcpy(digimon4.name, "nemvagas");
+    digimon4.age = 7;
+    digimon4.health = 55;
+    strcpy(digimon4.tamer_name, "mittudomen");
+    digimon4.digivolutionLevel = CHAMPION;
+    digimons[3] = digimon4;
 
-    printf("%d\n", same_trainer(&digimon_array[array_length], 3, "mittudomen"));
+
+    printf("%d\n", get_minimum_hp(digimons, 4));
+
+    printf("%d\n", same_digivolution_level(digimons, 4, ROOKIE));
+
+    printf("%d\n", same_trainer(digimons, 4, "mittudomen"));
+
+    printf("%d\n", average_hp_of_same_trainer(digimons, 4, "mittudomen"));
+
 
     return 0;
 
 
 }
 
-int get_minimum_hp(struct digimon digimon_array[array_length], int array_size) {
+int get_minimum_hp(digimon_t *digimon_array, int array_size) {
     array_length = array_size;
     int lovest_hp = 0;
-    digimon_array[0].health = lovest_hp;
+    lovest_hp = digimon_array[0].health;
     for (int i = 0; i < array_length; ++i) {
         if (digimon_array[i].health < lovest_hp) {
-            digimon_array[i].health = lovest_hp;
+            lovest_hp = digimon_array[i].health;
         }
 
     }
     return lovest_hp;
 }
 
-int same_digivolution_level(struct digimon *digimon_array, int array_size, enum digivolution_level digivolutionLevel) {
+int same_digivolution_level(digimon_t *digimon_array, int array_size, enum digivolution_level digivolutionLevel) {
     array_length = array_size;
     int counter = 0;
 
     for (int i = 0; i < array_length; ++i) {
         if (digimon_array[i].digivolutionLevel == digivolutionLevel) {
             counter++;
+
         }
 
     }
     return counter;
 }
 
-int same_trainer(struct digimon *digimon_array, int array_size, char tamer_name[256]) {
+int same_trainer(digimon_t *digimon_array, int array_size, char tamer_name[256]) {
     array_length = array_size;
     int counter = 0;
 
     for (int i = 0; i < array_length; ++i) {
         if (strcmp(digimon_array[i].tamer_name, tamer_name) == 0) {
-            counter++;
+
+            counter += 1;
         }
 
 
     }
     return counter;
+}
+
+int average_hp_of_same_trainer(digimon_t *digimon_array, int array_size, char tamer_name[256]) {
+    array_length = array_size;
+    int hp_counter = 0;
+    int digimon_counter = 0;
+    for (int i = 0; i < array_size; ++i) {
+
+            if (strcmp(digimon_array[i].tamer_name, tamer_name) == 0) {
+                hp_counter += digimon_array[i].health;
+                digimon_counter++;
+
+            }/* else if (strcmp(digimon_array[i].tamer_name, tamer_name) < 0) {
+                printf("no tamer like your input,or smthing went wrong\n");
+            } else // ((strcmp(digimon_array[i].tamer_name, tamer_name) > 0))
+            {
+                printf("no tamer like your input, or smthing went wrong\n");
+            }*/
+        }
+
+    return (hp_counter / digimon_counter);
+
 }
